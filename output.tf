a24e32b0b794a238bb4e569e2e07f2af5cbfa941
syreddy <syreddy@gmail.com>
2020-03-19 18:51:55 -0400

    update alb as part of chapter2

--- output.tf
+++ output.tf
@@ -1,7 +1,4 @@
-output "arn"{
-    value = aws_instance.example.arn
-}
 
-output "public_ip"{
-    value = aws_instance.example.public_ip
+output "alb_dns_name"{
+    value = aws_lb.example.dns_name
 }
